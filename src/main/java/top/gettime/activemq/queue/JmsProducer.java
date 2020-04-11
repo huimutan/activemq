0a8db65d2a7b14465c3016246974b3d951ba4cac
huimutan@bgs <xingdian119@hotmail.com>
2020-04-11 09:07:12 +0800

    加入topic

--- src/main/java/top/gettime/activemq/queue/JmsProducer.java
+++ src/main/java/top/gettime/activemq/queue/JmsProducer.java
@@ -1,19 +1,18 @@
-package top.gettime.activemq;
+package top.gettime.activemq.queue;
 
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
-import java.io.IOException;
 
 /**
  *
- * @Description: java原生ActiveMQ的API的使用-JMS消息消费者
+ * @Description: java原生ActiveMQ的API的使用-JMS消息生产者
  * @author leeSmall
  * @date 2018年9月13日
  *
  */
-public class JmsConsumer {
+public class JmsProducer {
 
     //默认连接用户名
     private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;
@@ -21,9 +20,12 @@ public class JmsConsumer {
     private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
     //默认连接地址
     //private static final String BROKEURL = ActiveMQConnection.DEFAULT_BROKER_URL;
-    private static final String BROKEURL = "tcp://192.168.11.30:61616";
+    private static final String BROKEURL = "tcp://172.24.0.111:61616";
     //提问名
-    private static final String QUEUE_NAME="firstMSG_huimutan";
+    private static final String NAME="MSG_huimutan";
+    //发送的消息数量
+    private static final int SENDNUM = 10;
+
     //编码过程参考JMS对象模型的几个要素
     public static void main(String[] args) {
         //1.连接工厂:创建一个JMS连接
@@ -38,21 +40,21 @@ public class JmsConsumer {
         //4.JMS目的:消息队列
         Destination destination;
 
-        //5.JMS消费者：接收和消费消息
-        MessageConsumer messageConsumer;
+        //5.JMS生产者:消息的生成
+        MessageProducer messageProducer;
 
-        //创建一个ActiveMQ的连接工厂
+        //第一步：创建一个ActiveMQ的连接工厂
         connectionFactory = new ActiveMQConnectionFactory(USERNAME,PASSWORD,BROKEURL);
 
         try {
-            //通过连接工厂创建一个JMS连接
+            //第二步：通过连接工厂创建一个JMS连接
             connection = connectionFactory.createConnection();
 
             //开启JMS连接
             connection.start();
 
             /*
-             * 通过JMS连接创建一个JMS会话
+             * 第三步：通过JMS连接创建一个JMS会话
              *
              * createSession参数取值说明：
              *  第一个参数：为true表示启用事务
@@ -64,40 +66,29 @@ public class JmsConsumer {
              * 消息可能会重复发送 在第二次重新传送消息的时候，消息头的JmsDelivered会被置为true标示当前消息已经传送过一次，
              * 客户端需要进行消息的重复处理控制。
              */
-            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            session = connection.createSession(true,Session.AUTO_ACKNOWLEDGE);
 
-            //通过JMS会话创建一个JMS目的，即消息队列
-            destination = session.createQueue(QUEUE_NAME);
+            //第四步：通过JMS会话创建一个JMS目的，即消息队列或者主题topic
+            destination = session.createQueue(NAME);
 
-            //通过JMS会话和JMS目的创建一个JMS消费者，即消息消费者
-            messageConsumer = session.createConsumer(destination);
+            //第五步：通过JMS会话和JMS目的创建一个JMS生产者，即消息生产者
+            messageProducer = session.createProducer(destination);
 
-//            //读取消息
-//            while(true){
-//                //使用receive方法消费一个消息，如果超过10s没有得到消息就跳过
-//                TextMessage textMessage = (TextMessage)messageConsumer.receive();//如果没有参数就一直等(l:10000)
-//                if(textMessage != null){
-//                    System.out.println("消费者接收的消息 : "+textMessage.getText());
-//                }else{
-//                    break;
-//                }
-//            }
-            messageConsumer.setMessageListener(new MessageListener() {
-                @Override
-                public void onMessage(Message message) {
-                    if (message != null && message instanceof TextMessage ) {
-                        TextMessage textMessage=(TextMessage) message;
-                        try {
-                            System.out.println("消费者接收的消息 : "+textMessage.getText());
-                        } catch (JMSException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            });
-            System.in.read();//如果用监听器这种写法，需要使用read（）等待程序运行。
+            //第六步：发送10条消息
+            for(int i=1;i<=SENDNUM;i++){
+                //第七步：生成消息
+                String msg = "消息"+i+" "+System.currentTimeMillis();
+                TextMessage message = session.createTextMessage(msg);
+                System.out.println("发送消息:"+msg);
 
-        } catch (JMSException | IOException e) {
+                //第八步：发送消息
+                messageProducer.send(message);
+            }
+            //提交JMS会话，原视频没有
+            session.commit();
+
+
+        } catch (JMSException e) {
             e.printStackTrace();
         }finally {
             if(connection!=null){
@@ -106,7 +97,7 @@ public class JmsConsumer {
                     //视频里说的messageProducer.close();
                     //视频里说的session.close();
                     connection.close();
-                    System.out.println("接收MQ消息完成");
+                    System.out.println("消息发布到MQ完成");
                 } catch (JMSException e) {
                     e.printStackTrace();
                 }
@@ -114,6 +105,7 @@ public class JmsConsumer {
         }
 
 
-
     }
+
+
 }
